986b87c684c52940bbf781f0dcd5310459c4a340
MiguelSperle <miguelsperle205@yahoo.com>
2023-11-30 14:08:52 -0300

    updated archive (custom-message) imports

--- src/app/errors/user-not-found/components/custom-message-not-found/custom-message.tsx
+++ src/app/errors/user-not-found/components/custom-message-not-found/custom-message.tsx
@@ -1,31 +1,36 @@
+import Button from '@/components/Button'
+import Typography from '@/components/Typography'
+import Link from 'next/link'
+import { HiChevronLeft } from 'react-icons/hi'
+import * as Styled from './styles'
+
 export default function CustomMessage() {
   return (
-    <p>ola</p>
-    // <Styled.ContainerCustomError404>
-    //   <Link href="/">
-    //     <Button
-    //       width="50px"
-    //       height="50px"
-    //       fontSize="1.5rem"
-    //       className="button-chevron-left"
-    //     >
-    //       <HiChevronLeft />
-    //     </Button>
-    //   </Link>
+    <Styled.ContainerCustomError404>
+      <Link href="/">
+        <Button
+          width="50px"
+          height="50px"
+          fontSize="1.5rem"
+          className="button-chevron-left"
+        >
+          <HiChevronLeft />
+        </Button>
+      </Link>
 
-    //   <Typography text="Opss.." fontSize="1.5rem" />
-    //   <Typography text="User doesn`t exist, back to finder" fontSize="1rem" />
+      <Typography text="Opss.." fontSize="1.5rem" />
+      <Typography text="User doesn`t exist, back to finder" fontSize="1rem" />
 
-    //   <Link href="/">
-    //     <Button
-    //       width="150px"
-    //       height="50px"
-    //       fontSize="1.25rem"
-    //       className="button-go-find"
-    //     >
-    //       FIND
-    //     </Button>
-    //   </Link>
-    // </Styled.ContainerCustomError404>
+      <Link href="/">
+        <Button
+          width="150px"
+          height="50px"
+          fontSize="1.25rem"
+          className="button-go-find"
+        >
+          FIND
+        </Button>
+      </Link>
+    </Styled.ContainerCustomError404>
   )
 }
