40e9d2d1191d3a58e478d0d1b1a5513c423eea84
MiguelSperle <miguelsperle205@yahoo.com>
2023-11-30 14:01:08 -0300

    commented (custom-message) test

--- src/app/errors/user-not-found/components/custom-message-not-found/custom-message.tsx
+++ src/app/errors/user-not-found/components/custom-message-not-found/custom-message.tsx
@@ -2,35 +2,36 @@ import Button from '@/components/Button'
 import Typography from '@/components/Typography'
 import Link from 'next/link'
 import { HiChevronLeft } from 'react-icons/hi'
-import * as Styled from '../custom-message-not-found/styles'
+import * as Styled from './styles'
 
 export default function CustomMessage() {
   return (
-    <Styled.ContainerCustomError404>
-      <Link href="/">
-        <Button
-          width="50px"
-          height="50px"
-          fontSize="1.5rem"
-          className="button-chevron-left"
-        >
-          <HiChevronLeft />
-        </Button>
-      </Link>
+    <p>ola</p>
+    // <Styled.ContainerCustomError404>
+    //   <Link href="/">
+    //     <Button
+    //       width="50px"
+    //       height="50px"
+    //       fontSize="1.5rem"
+    //       className="button-chevron-left"
+    //     >
+    //       <HiChevronLeft />
+    //     </Button>
+    //   </Link>
 
-      <Typography text="Opss.." fontSize="1.5rem" />
-      <Typography text="User doesn`t exist, back to finder" fontSize="1rem" />
+    //   <Typography text="Opss.." fontSize="1.5rem" />
+    //   <Typography text="User doesn`t exist, back to finder" fontSize="1rem" />
 
-      <Link href="/">
-        <Button
-          width="150px"
-          height="50px"
-          fontSize="1.25rem"
-          className="button-go-find"
-        >
-          FIND
-        </Button>
-      </Link>
-    </Styled.ContainerCustomError404>
+    //   <Link href="/">
+    //     <Button
+    //       width="150px"
+    //       height="50px"
+    //       fontSize="1.25rem"
+    //       className="button-go-find"
+    //     >
+    //       FIND
+    //     </Button>
+    //   </Link>
+    // </Styled.ContainerCustomError404>
   )
 }
