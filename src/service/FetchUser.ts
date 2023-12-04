1fad348b10622129755a83038c20a218f7c3784c
MiguelSperle <miguelsperle205@yahoo.com>
2023-12-04 18:50:10 -0300

    Updated file/service/FetchUser.ts

--- src/service/FetchUser.ts
+++ src/service/FetchUser.ts
@@ -5,7 +5,7 @@ export async function FetchUser(name: string) {
   if (!name) return null
 
   const response = await fetch(`https://api.github.com/users/${name}`, {
-    next: { revalidate: 300 }, // Até bater esse tempo ele vai usar o cache que foi salvo na primeira fetch, depos que bater o mesmo, ele refaz a chamada e salva novamente em cache e fica nesse ciclo.(A CADA 1 HORA)
+    next: { revalidate: 300 }, // Até bater esse tempo ele vai usar o cache que foi salvo na primeira fetch, depos que bater o mesmo, ele refaz a chamada e salva novamente em cache e fica nesse ciclo.(A CADA 5 minutos)
   })
 
   if (!response.ok && response.status === 404) {
